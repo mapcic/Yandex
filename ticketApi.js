8c011940780aa368f2f181db76bb4e051a0c8621
mapcic <mapcic@mail.ru>
2017-09-04 12:13:37 +0300

    Init TicketApi

---
+++ ticketApi.js
@@ -0,0 +1,11 @@
+// Control ticket
+function TicketContol() {}
+
+// Class to figure tiket
+function TicketForm() {}
+
+// Tiket Class
+function Ticket() {}
+
+// Vehicles class
+function Vehicle() {}
